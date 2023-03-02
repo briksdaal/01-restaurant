de20f3a2deb9a4fe159bc7ed5712e20d26b9692e
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-03-02 15:31:59 +0200

    Configure webpack for fonts and images

--- src/home.js
+++ src/home.js
@@ -1,6 +1,13 @@
+import falafelImg from './images/falafel.avif';
+
 function getHome() {
   const home = document.createElement('div');
   home.textContent = 'Home sweet home';
+
+  const falafel = new Image();
+  falafel.src = falafelImg;
+
+  home.appendChild(falafel);
 
   return home;
 }
